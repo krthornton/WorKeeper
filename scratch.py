9c2317134ed53700c5629ed32e658cde9971174f
Kaleb Thornton <kaleb.thornton2@gmail.com>
2018-02-24 23:32:21 -0600

    No code change; scratch contains json testing; added testfile.json

--- scratch.py
+++ scratch.py
@@ -1,9 +1,28 @@
 
 
-output = []
-output.append('{:>12}'.format("Assignment: ") + "%s\n" % "swag")
-output.append('{:>12}'.format("Due: ") + "%s\n" % "swag")
-output.append('{:>12}'.format("Subject: ") + "%s\n" % "swag")
-output.append('{:>12}'.format("Completed: ") + "%s\n" % "swag")
-for i in output:
-    print(i, end="")
+import json
+from assignment import *
+
+
+"""
+class BigDic:
+    def __init__(self):
+        self.__dict = {"Default" : "BigDic"}
+
+    def get_dic(self):
+        return self.__dict
+"""
+
+# create and convert dictionary
+big_dict = Assignment("Testing", "123", "Something", True)
+dict_json = json.dumps(big_dict.info())
+
+# open file to save
+write_file = open("testfile.json", "w")
+json.dump(dict_json, write_file)
+write_file.close()
+
+# open file to read
+read_file = open("testfile.json", "r")
+data = json.load(read_file)
+print(data)
